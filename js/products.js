bca3942c5082d3ac393415f9abe5b8c03be135f7
sumit1516-stack <sumitkumarcs1@gmail.com>
2021-02-22 19:16:52 +0000

    first commit

---
+++ js/products.js
@@ -0,0 +1,404 @@
+var products = {
+        "items": [{
+            "eid": "ashantinomad",
+            "name": "Ashanti Nomad",
+            "categoryId": "men,men:shirts",
+            "message": "The Ashanti is a great looking tee made from high-quality, durable material.",
+            "thumbnailUrl": "/shell/en/men/shirts/ashanti-nomad/_jcr_content/par/product/image.nav.jpg",
+            "value": "29.5",
+            "pageUrl": "/shell/en/men/shirts/ashanti-nomad.html"
+
+        }, {
+            "eid": "bambaracargo",
+            "name": "Bambara Cargo",
+            "categoryId": "men,men:shirts",
+            "message": "The Bambara tee is made with a light weave of pure cotton.",
+            "thumbnailUrl": "/shell/en/men/shirts/bambara-cargo/_jcr_content/par/product/image.nav.jpg",
+            "value": "12.5",
+            "pageUrl": "/shell/en/men/shirts/bambara-cargo.html"
+
+        }, {
+            "eid": "edmontonwinter",
+            "name": "Edmonton Winter",
+            "categoryId": "men,men:coats,equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/men/coats/edmonton-winter/_jcr_content/par/product/image.nav.jpg",
+            "value": "160",
+            "pageUrl": "/shell/en/men/coats/edmonton-winter.html"
+
+        }, {
+            "eid": "fulaninomad",
+            "name": "Fulani Nomad",
+            "categoryId": "men,men:pants,equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/men/pants/fulani-nomad/_jcr_content/par/product/image.nav.jpg",
+            "value": "59.9",
+            "pageUrl": "/shell/en/men/pants/fulani-nomad.html"
+
+        }, {
+            "eid": "jolasummer",
+            "name": "Jola Summer",
+            "categoryId": "men,men:shorts",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/men/shorts/jola-summer/_jcr_content/par/product/image.nav.jpg",
+            "value": "47",
+            "pageUrl": "/shell/en/men/shorts/jola-summer.html"
+
+        }, {
+            "eid": "markasport",
+            "name": "Marka Sport",
+            "categoryId": "men,men:shorts,equipment,equipment:running",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/men/shorts/marka-sport/_jcr_content/par/product/image.nav.jpg",
+            "value": "45",
+            "pageUrl": "/shell/en/men/shorts/marka-sport.html"
+
+        }, {
+            "eid": "tuaregsummer",
+            "name": "Tuareg Summer",
+            "categoryId": "men,men:shorts,equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/men/shorts/tuareg-summer/_jcr_content/par/product/image.nav.jpg",
+            "value": "39.9",
+            "pageUrl": "/shell/en/men/shorts/tuareg-summer/_jcr_content/par/product/image.nav.jpg"
+
+        }, {
+            "eid": "borabora",
+            "name": "Bora Bora",
+            "categoryId": "women,women:shirts,equipment,equipment:running",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/shirts/bora-bora/_jcr_content/par/product/image.nav.jpg",
+            "value": "38",
+            "pageUrl": "/shell/en/women/shirts/bora-bora.html"
+
+        }, {
+            "eid": "calgarywinter",
+            "name": "Calgary Winter",
+            "categoryId": "women,women:coats,equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/coats/calgary-winter/_jcr_content/par/product/image.nav.jpg",
+            "value": "280",
+            "pageUrl": "/shell/en/women/coats/calgary-winter.html"
+
+        }, {
+            "eid": "fijisport",
+            "name": "Fiji Sport",
+            "categoryId": "women,women:shorts,equipment,equipment:running",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/shorts/fiji-sport/_jcr_content/par/product/image.nav.jpg",
+            "value": "32",
+            "pageUrl": "/shell/en/women/shorts/fiji-sport.html"
+
+        }, {
+            "eid": "mauimarine",
+            "name": "Maui Marine",
+            "categoryId": "women,women:shirts",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/shirts/maui-marine/_jcr_content/par/product/image.nav.jpg",
+            "value": "29.9",
+            "pageUrl": "/shell/en/women/shirts/maui-marine.html"
+
+        }, {
+            "eid": "palausummer",
+            "name": "Palau Summer",
+            "categoryId": "women,women:shirts",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/shirts/palau-summer/_jcr_content/par/product/image.nav.jpg",
+            "value": "55",
+            "pageUrl": "/shell/en/women/shirts/palau-summer.html"
+
+        }, {
+            "eid": "saskatoonparka",
+            "name": "Saskatoon Parka",
+            "categoryId": "women,women:coats,equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/coats/saskatoon-parka/_jcr_content/par/product/image.nav.jpg",
+            "value": "299",
+            "pageUrl": "/shell/en/women/coats/saskatoon-parka.html"
+
+        }, {
+            "eid": "tahitisummer",
+            "name": "Tahiti Summer",
+            "categoryId": "women,women:shorts",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/shorts/tahiti-summer/_jcr_content/par/product/image.nav.jpg",
+            "value": "49.9",
+            "pageUrl": "/shell/en/women/shorts/tahiti-summer.html"
+
+        }, {
+            "eid": "tongafashion",
+            "name": "Tonga Fashion",
+            "categoryId": "women,women:pants",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/pants/tonga-fashion/_jcr_content/par/product/image.nav.jpg",
+            "value": "45",
+            "pageUrl": "/shell/en/women/pants/tonga-fashion.html"
+
+        }, {
+            "eid": "tupaisummer",
+            "name": "Tupai Summer",
+            "categoryId": "women,women:shirts",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/women/shirts/tupai-summer/_jcr_content/par/product/image.nav.jpg",
+            "value": "33",
+            "pageUrl": "/shell/en/women/shirts/tupai-summer.html"
+
+        }, {
+            "eid": "abidjanwater",
+            "name": "Abidjan Water",
+            "categoryId": "equipment,equipment:swimming",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/swimming/abidjan-water/_jcr_content/par/product/image.nav.jpg",
+            "value": "38.9",
+            "pageUrl": "/shell/en/equipment/swimming/abidjan-water.html"
+
+        }, {
+            "eid": "baffinsnow",
+            "name": "Baffin Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/baffin-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "38",
+            "pageUrl": "/shell/en/equipment/skiing/baffin-snow.html"
+
+        }, {
+            "eid": "banffsnow",
+            "name": "Banff Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/banff-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "220",
+            "pageUrl": "/shell/en/equipment/skiing/banff-snow.html"
+
+        }, {
+            "eid": "blackcombsnow",
+            "name": "Blackcomb Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/blackcomb-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "75",
+            "pageUrl": "/shell/en/equipment/skiing/blackcomb-snow.html"
+
+        }, {
+            "eid": "brazzaville",
+            "name": "Brazzaville",
+            "categoryId": "equipment,equipment:swimming",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/swimming/brazzaville/_jcr_content/par/product/image.nav.jpg",
+            "value": "52",
+            "pageUrl": "/shell/en/equipment/swimming/brazzaville.html"
+
+        }, {
+            "eid": "cajamara",
+            "name": "Cajamara",
+            "categoryId": "equipment,equipment:biking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/biking/cajamara/_jcr_content/par/product/image.nav.jpg",
+            "value": "610",
+            "pageUrl": "/shell/en/equipment/biking/cajamara.html"
+
+        }, {
+            "eid": "cuzco",
+            "name": "Cuzco",
+            "categoryId": "equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/hiking/cuzco/_jcr_content/par/product/image.nav.jpg",
+            "value": "210",
+            "pageUrl": "/shell/en/equipment/hiking/cuzco.html"
+
+        }, {
+            "eid": "davostrek",
+            "name": "Davos Trek",
+            "categoryId": "equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/hiking/davos-trek/_jcr_content/par/product/image.nav.jpg",
+            "value": "15",
+            "pageUrl": "/shell/en/equipment/hiking/davos-trek.html"
+
+        }, {
+            "eid": "ferniesnow",
+            "name": "Fernie Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/fernie-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "38",
+            "pageUrl": "/shell/en/equipment/skiing/fernie-snow.html"
+
+        }, {
+            "eid": "halifaxwinter",
+            "name": "Halifax Winter",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/halifax-winter/_jcr_content/par/product/image.nav.jpg",
+            "value": "22",
+            "pageUrl": "/shell/en/equipment/skiing/halifax-winter.html"
+
+        }, {
+            "eid": "interlakentrek",
+            "name": "Interlaken Trek",
+            "categoryId": "equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/hiking/interlaken-trek/_jcr_content/par/product/image.nav.jpg",
+            "value": "189.9",
+            "pageUrl": "/shell/en/equipment/hiking/interlaken-trek.html"
+
+        }, {
+            "eid": "irianjaya",
+            "name": "Irian Jaya",
+            "categoryId": "equipment,equipment:sunglasses",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/sunglasses/irian-jaya/_jcr_content/par/product/image.nav.jpg",
+            "value": "37",
+            "pageUrl": "/shell/en/equipment/sunglasses/irian-jaya.html"
+
+        }, {
+            "eid": "kamloopssnow",
+            "name": "Kamloops Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/kamloops-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "190",
+            "pageUrl": "/shell/en/equipment/skiing/kamloops-snow.html"
+
+        }, {
+            "eid": "kawarthasnow",
+            "name": "Kawartha Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/kawartha-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "82",
+            "pageUrl": "/shell/en/equipment/skiing/kawartha-snow.html"
+
+        }, {
+            "eid": "kelownasnow",
+            "name": "Kelowna Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/kelowna-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "38",
+            "pageUrl": "/shell/en/equipment/skiing/kelowna-snow.html"
+
+        }, {
+            "eid": "lagosminilongboard",
+            "name": "Lagos Mini Longboard",
+            "categoryId": "equipment,equipment:surfing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/surfing/lagos-mini-longboard/_jcr_content/par/product/image.nav.jpg",
+            "value": "319.9",
+            "pageUrl": "/shell/en/equipment/surfing/lagos-mini-longboard.html"
+
+        }, {
+            "eid": "longirodtrek",
+            "name": "Longirod Trek",
+            "categoryId": "equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/hiking/longirod-trek/_jcr_content/par/product/image.nav.jpg",
+            "value": "89",
+            "pageUrl": "/shell/en/equipment/hiking/longirod-trek.html"
+
+        }, {
+            "eid": "mombassarunners",
+            "name": "Mombassa Runners",
+            "categoryId": "equipment,equipment:running",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/running/mombassa-runners/_jcr_content/par/product/image.nav.jpg",
+            "value": "120",
+            "pageUrl": "/shell/en/equipment/running/mombassa-runners.html"
+
+        }, {
+            "eid": "monttremblant",
+            "name": "Mont Tremblant",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/mont-tremblant/_jcr_content/par/product/image.nav.jpg",
+            "value": "67",
+            "pageUrl": "/shell/en/equipment/skiing/mont-tremblant.html"
+
+        }, {
+            "eid": "montrealsnow",
+            "name": "Montreal Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/montreal-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "29.9",
+            "pageUrl": "/shell/en/equipment/skiing/montreal-snow.html"
+
+        }, {
+            "eid": "nairobirunners",
+            "name": "Nairobi Runners",
+            "categoryId": "equipment,equipment:running",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/running/nairobi-runners/_jcr_content/par/product/image.nav.jpg",
+            "value": "75",
+            "pageUrl": "/shell/en/equipment/running/nairobi-runners.html"
+
+        }, {
+            "eid": "nunavutfleece",
+            "name": "Nunavut Fleece",
+            "categoryId": "equipment,equipment:hiking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/hiking/nunavut-fleece/_jcr_content/par/product/image.nav.jpg",
+            "value": "150",
+            "pageUrl": "/shell/en/equipment/hiking/nunavut-fleece.html"
+
+        }, {
+            "eid": "rajaampat",
+            "name": "Raja Ampat",
+            "categoryId": "equipment,equipment:sunglasses",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/sunglasses/raja-ampat/_jcr_content/par/product/image.nav.jpg",
+            "value": "36",
+            "pageUrl": "/shell/en/equipment/sunglasses/raja-ampat/_jcr_content/par/product/image.nav.jpg"
+
+        }, {
+            "eid": "sparrow",
+            "name": "Sparrow",
+            "categoryId": "equipment,equipment:biking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/biking/sparrow/_jcr_content/par/product/image.nav.jpg",
+            "value": "1250",
+            "pageUrl": "/shell/en/equipment/biking/sparrow.html"
+
+        }, {
+            "eid": "sumatra",
+            "name": "Sumatra",
+            "categoryId": "equipment,equipment:sunglasses",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/sunglasses/sumatra/_jcr_content/par/product/image.nav.jpg",
+            "value": "39.99",
+            "pageUrl": "/shell/en/equipment/sunglasses/sumatra.html"
+
+        }, {
+            "eid": "tacna",
+            "name": "Tacna",
+            "categoryId": "equipment,equipment:biking",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/biking/tacna/_jcr_content/par/product/image.nav.jpg",
+            "value": "62",
+            "pageUrl": "/shell/en/equipment/biking/tacna.html"
+        }, {
+            "eid": "timor",
+            "name": "Timor",
+            "categoryId": "equipment,equipment:sunglasses",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/sunglasses/timor/_jcr_content/par/product/image.nav.jpg",
+            "value": "24",
+            "pageUrl": "/shell/en/equipment/sunglasses/timor.html"
+        }, {
+            "eid": "tobermorysnow",
+            "name": "Tobermory Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/tobermory-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "248",
+            "pageUrl": "/shell/en/equipment/skiing/tobermory-snow.html"
+        }, {
+            "eid": "whistlersnow",
+            "name": "Whistler Snow",
+            "categoryId": "equipment,equipment:skiing",
+            "message": "This is a brand new product",
+            "thumbnailUrl": "/shell/en/equipment/skiing/whistler-snow/_jcr_content/par/product/image.nav.jpg",
+            "value": "340",
+            "pageUrl": "/shell/en/equipment/skiing/whistler-snow.html"
+        }]
+}
